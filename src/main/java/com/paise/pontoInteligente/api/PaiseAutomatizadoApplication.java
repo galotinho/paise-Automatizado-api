f37a13850911598f7522ea4d8d5a8b900d011937
Rafael <galotinhodelta@gmail.com>
2019-07-01 20:16:11 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/paise/pontoInteligente/api/PaiseAutomatizadoApplication.java
@@ -0,0 +1,13 @@
+package com.paise.pontoInteligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiseAutomatizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiseAutomatizadoApplication.class, args);
+	}
+
+}
