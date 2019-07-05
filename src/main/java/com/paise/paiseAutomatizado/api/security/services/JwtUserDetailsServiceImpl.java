479c884a09e44d7cf36f6298a193ae398b3d76cd
Rafael <galotinhodelta@gmail.com>
2019-07-05 14:33:22 -0300

    Adicionado securança com token jwt

---
+++ src/main/java/com/paise/paiseAutomatizado/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.paise.paiseAutomatizado.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.paise.paiseAutomatizado.api.entidades.Funcionario;
+import com.paise.paiseAutomatizado.api.security.JwtUserFactory;
+import com.paise.paiseAutomatizado.api.servicos.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
