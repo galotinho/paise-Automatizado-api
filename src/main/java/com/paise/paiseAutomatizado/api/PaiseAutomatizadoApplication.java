33fd040f021b1d490e5e912caf606eb5d7b3e146
Rafael <galotinhodelta@gmail.com>
2019-07-10 11:39:18 -0300

    Autenticação via token do Swagger2 corrigido

--- src/main/java/com/paise/paiseAutomatizado/api/PaiseAutomatizadoApplication.java
+++ src/main/java/com/paise/paiseAutomatizado/api/PaiseAutomatizadoApplication.java
@@ -2,8 +2,10 @@ package com.paise.paiseAutomatizado.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PaiseAutomatizadoApplication {
 
 	public static void main(String[] args) {
